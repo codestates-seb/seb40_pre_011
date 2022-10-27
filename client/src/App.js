857a1822ceb02279594b8a8b9af72d69334f1227
likeprograming1 <hsj2216@naver.com>
2022-10-27 16:16:39 +0900

    test3

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,7 @@
 import "./App.css";
 
 function App() {
-<<<<<<< HEAD
-  return <div className="App">dass</div>;
-=======
-  return <div className="App">123</div>;
->>>>>>> e0330e183d7f1063cb732a21d3de559d98357fe7
+  return <div className="App">123ㅁㅁㅁ</div>;
 }
 
 export default App;
