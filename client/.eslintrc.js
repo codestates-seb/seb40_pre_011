5fb0c390645148027ab766e50831fb13cb5fcd50
likeprograming1 <hsj2216@naver.com>
2022-10-28 10:59:00 +0900

    setting

---
+++ client/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb', 'eslint:recommended', 'plugin:prettier/recommended'],
+  rules: {
+    'react/button-has-type': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.js', '.jsx'],
+      },
+    ],
+    'no-console': 0,
+  },
+};
--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
+import Mainpage from './pages/Mainpage';
 
 function App() {
-  return <Routes>
-    <Route path="/" />
-    <Route path="/login" />
-    <Route path="/logout" />
-    <Route path="/join" />
-    <Route path="/questionBoard" />
-    <Route path="/questionBoard/:id" />
-  </Routes>
+  return (
+    <Routes>
+      <Route path="/" element={<Mainpage />} />
+      <Route path="/login" />
+      <Route path="/logout" />
+      <Route path="/join" />
+      <Route path="/questionBoard" />
+      <Route path="/questionBoard/:id" />
+    </Routes>
+  );
 }
 
 export default App;
