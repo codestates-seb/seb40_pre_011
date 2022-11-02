ae9bf6270603bbf6a2310bc5dac143c5c0941adb
kuku <covernat6949@gmail.com>
2022-11-02 10:22:38 +0900

    feat#42 proxy example delete

--- client/src/pages/Login/LoginPage.jsx
+++ client/src/pages/Login/LoginPage.jsx
@@ -1,12 +1,8 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 import Header from '../../components/Header/Header';
 import Login from '../../components/Login/Login';
 
 function LoginPage() {
-  useEffect(() => {
-    axios.get('/api/').then(data => console.log(data));
-  });
   return (
     <>
       <Header />
