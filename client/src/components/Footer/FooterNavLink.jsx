536283633fc2886cd1a861dd3a24bbde7d74bf38
kuku <covernat6949@gmail.com>
2022-11-04 20:42:09 +0900

    feat footer modify

---
+++ client/src/components/Footer/FooterNavLink.jsx
@@ -0,0 +1,36 @@
+const FooterNavData = [
+  { title: 'STACKOVERFLOW', list: ['Questions', 'Help'] },
+  {
+    title: 'PRODUCTS',
+    list: ['Teams', 'Advertising', 'Collectives', 'Talent'],
+  },
+  {
+    title: 'COMPANY',
+    list: [
+      'About',
+      'Press',
+      'Work Here',
+      'Legal',
+      'Privacy Policy',
+      'Terms of Service',
+      'Contact Us',
+      'Cookie Settings',
+      'Cookie Policy',
+    ],
+  },
+  {
+    title: 'STACK EXCHANGE NETWORK',
+    list: [
+      'Technology',
+      'Culture & recreation',
+      'Life & arts',
+      'Science',
+      'Professional',
+      'Business',
+      'API',
+      'Data',
+    ],
+  },
+];
+
+export default FooterNavData;
