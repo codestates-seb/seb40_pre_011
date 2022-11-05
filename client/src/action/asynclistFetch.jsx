11afc4e5854adb406760af9b932b436b3ce1753b
likeprograming1 <hsj2216@naver.com>
2022-11-05 23:34:04 +0900

    das

--- client/src/action/asynclistFetch.jsx
+++ client/src/action/asynclistFetch.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const asynclistFetch = createAsyncThunk('list/data', async page => {
   const data = await axios.get(`/content?page=${page}&size=10`).then(res => {
+    console.log(res);
     return res.data;
   });
   return data;
