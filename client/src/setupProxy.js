39a7cc6c5c904cea153935cf95926950ae5b9cb4
likeprograming1 <hsj2216@naver.com>
2022-11-06 01:31:29 +0900

    수정

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,14 +1,18 @@
-const { createProxyMiddleware } = require('http-proxy-middleware'); // eslint-disable-line no-unused-vars
-
+const { createProxyMiddleware } = require('http-proxy-middleware');
+// eslint-disable-line no-unused-vars
+const TARGET_URL =
+  'http://ec2-3-38-107-169.ap-northeast-2.compute.amazonaws.com:8080/';
+const API_URL =
+  'http://ec2-3-36-121-198.ap-northeast-2.compute.amazonaws.com:8080/';
 module.exports = app => {
   app.use(
     createProxyMiddleware(['/member', '/content', '/signup', '/user'], {
-      target: process.env.REACT＿APP＿TARGET_URL,
+      target: TARGET_URL,
       changeOrigin: true,
       router: {
-        '/content': process.env.REACT_APP_API_URL,
-        '/user': process.env.REACT＿APP＿TARGET_URL,
-        '/signup': process.env.REACT＿APP＿TARGET_URL,
+        '/content': API_URL,
+        '/user': TARGET_URL,
+        '/signup': TARGET_URL,
       },
     }),
   );
